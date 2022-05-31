0a04468188c1b38f6a4966a7b58f7492eb3fdca0
Kim-yejin-yj <kyj062157@gmail.com>
2022-05-31 23:10:40 +0900

    1234

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button developer_info_btn = (Button) findViewById(R.id.button5);
+        developer_info_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), mypalnt.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
