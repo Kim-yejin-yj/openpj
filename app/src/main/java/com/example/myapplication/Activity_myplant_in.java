ccb5281f43417487478cb106d70eb598a0d75b50
Kim-yejin-yj <kyj062157@gmail.com>
2022-06-01 22:30:23 +0900

    다시

---
+++ app/src/main/java/com/example/myapplication/Activity_myplant_in.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Activity_myplant_in extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_myplant_in);
+    }
+}
--- app/src/main/java/com/example/myapplication/mypalnt.java
+++ app/src/main/java/com/example/myapplication/mypalnt.java
@@ -1,6 +1,10 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class mypalnt extends AppCompatActivity {
@@ -10,9 +14,14 @@ public class mypalnt extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mypalnt);
 
+        Button developer_info_btn = (Button) findViewById(R.id.button4);
+        developer_info_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),Activity_myplant_in.class);
+                startActivity(intent);
+            }
+        });
     }
-
-
-
 }
 
